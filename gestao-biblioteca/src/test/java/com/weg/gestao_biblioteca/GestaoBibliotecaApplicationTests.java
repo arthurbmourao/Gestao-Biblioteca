1a1f3a192cd3efb11d117f8c6a193104455bf61c
arthur_b_mourao <arthur_b_mourao@estudante.sesisenai.org.br>
2026-06-30 09:38:22 -0300

    Inicializando projeto

---
+++ gestao-biblioteca/src/test/java/com/weg/gestao_biblioteca/GestaoBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weg.gestao_biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
